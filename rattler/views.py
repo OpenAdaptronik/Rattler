e38b76d6ebc514ae694870c9f2cd67787a441ddf
Phil <philjac@computer-heddesheim.de>
2017-11-25 18:14:24 +0100

    erste Schritte Richtung Integration des Frontends in Jinja und Co. es geht voran, Licht am Ende des Tunnels ist zu sehen.

--- rattler/views.py
+++ rattler/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index (request):
+    return render (request,'index.html')
+
+def dashboard (request):
+    return render (request,'dashboard/index.html')
+
+def error (request):
+    return render (request,'error404/index.html')
