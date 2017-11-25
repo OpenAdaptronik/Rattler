ae9023d116a43a194c98a726a1fde21d370c6c59
Phil <philjac@computer-heddesheim.de>
2017-11-25 18:14:24 +0100

    erste Schritte Richtung Integration des Frontends in Jinja und Co. es geht voran, Licht am Ende des Tunnels ist zu sehen.

--- rattler/urls.py
+++ rattler/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^', include('apps.index.urls')),
+    #url(r'/dashboard/', views.dashboard, name='dashboard'),
+    url(r'$', views.index, name='index'),
+    #url(r'^', views.error404, name='error404'),
     url(r'^admin/', admin.site.urls)
 ]
