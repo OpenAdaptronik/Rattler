97cd1e4a7edeeae0fdf9bb4c49f5cc77ae1eff25
Heggi95 <joerg.hergert@gmail.com>
2017-11-25 11:38:24 +0100

    cleanup

--- app/modules/counter/controllers/index.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app.modules.counter import module_counter
-from app.models.counter import Counter
-
-@module_counter.route('/')
-def index():
-    return render_template('counter.html', counter=Counter.query.all(), title='Counter')
--- app/tasks/run.py
+++
@@ -1,6 +0,0 @@
-from app.tasks import command
-from app import app
-
-@command
-def run():
-    app.run(debug=True, host='0.0.0.0')
