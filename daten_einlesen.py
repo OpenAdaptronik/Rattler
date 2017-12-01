1203a233b599d1f1a788e53bda1fff166db6c7be
Heggi95 <joerg.hergert@gmail.com>
2017-12-01 14:21:30 +0100

    Einlesen der CSV egal ob header oder nicht funktioniert

--- daten_einlesen.py
+++ daten_einlesen.py
@@ -2,22 +2,55 @@ import pandas as pd
 import numpy as np
 import scipy as sci
 
-header_found = False
+
+
+
 
 # Header = None -> ignoriert
 daten = pd.read_csv('CSV_files/multidata_equal_/single_multidata_equal_Time_data.csv')
-daten2 = pd.read_csv('CSV_files/multidata_equal_/none_multidata_equal_Time_data.csv', header=None)
+daten2 = pd.read_csv('CSV_files/multidata_equal_/none_multidata_equal_Time_data.csv')
 
 # Preview Daten
-print('Daten wurden erfolgreich eingelesen: \n\n', daten.head(10))
+#print('Daten wurden erfolgreich eingelesen: \n\n', daten2.head(10))
 
 colNames_User = []
 colUnits_User = []
 
 
+def header_Format(data):
+    """
+
+    :param data: the dataframe of the actuall csv - file
+    :return: dataframe with a header of
+    """
+    i = 0
+    colHeader = []
+
+
+
+    colValues = data.columns.values
+    if isinstance(data.columns.values,str):
+        return data
+    else:
+        for values in data.columns.values:
+            colHeader.append(i)
+            i += 1
+
+        print(colValues)
+        data.loc[-1] = colValues
+        data.index = data.index+1
+        data = data.sort_index()
+        data.columns = colHeader
+
+        return data
+
+
+
+
+
 # Fragt den Nutzer nach den Bezeichnern der Spalte und in welcher Einheit diese Daten gemessen wurde und fügt die Einheiten an idx = 0 ein
 def firstFormat(data):
-    i = -1
+    i = 0
     for value in list(data.head()):
         i += 1
         # Instance of Numpy.Int64 because of the fact that this is the standard type of an pandas Dataframe
@@ -43,5 +76,7 @@ def firstFormat(data):
     return data
 
 
-daten = firstFormat(daten2)
-print(daten)
+
+print(header_Format(daten2))
+print(daten.iloc[:,1])
+
