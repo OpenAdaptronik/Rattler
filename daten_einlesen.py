3ef05460e870db0d70e7a9ef1848565d0772cd08
Erik Schwan <schwandev@gmail.com>
2017-12-01 14:57:56 +0100

    - Abfragen laufen, nur pandas noch nicht

--- daten_einlesen.py
+++ daten_einlesen.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import scipy as sci
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
 
 
 
@@ -8,13 +10,14 @@ import scipy as sci
 
 # Header = None -> ignoriert
 daten = pd.read_csv('CSV_files/multidata_equal_/single_multidata_equal_Time_data.csv')
-daten2 = pd.read_csv('CSV_files/multidata_equal_/none_multidata_equal_Time_data.csv')
-
+daten2 = pd.read_csv('CSV_files/multidata_equal_/none_multidata_equal_Time_data.csv', header=None)
+masse = pd.read_csv('CSV_files/Massenschwinger/Simulation_3_Massenschwinger_Zeitdaten.txt')
 # Preview Daten
 #print('Daten wurden erfolgreich eingelesen: \n\n', daten2.head(10))
 
 colNames_User = []
 colUnits_User = []
+
 
 
 def header_Format(data):
@@ -50,6 +53,13 @@ def header_Format(data):
 
 # Fragt den Nutzer nach den Bezeichnern der Spalte und in welcher Einheit diese Daten gemessen wurde und fügt die Einheiten an idx = 0 ein
 def firstFormat(data):
+    '''
+    Die Methode fragt den Nutzer nach den Bezeichnern der Spalte ab und in welcher Einheit diese Daten gemessen wurden.
+    Die Bezeichner/Namen werden in den Head geschrieben und die Einheiten werden in die Zeile mit den Listenindex
+    idx = 0 geschrieben. Somit fangen die Werte erst ab Index 1 an, dies muss bei Berechnungen beachtet werden.
+    :param data: List mit
+    :return: Datenliste mit Bezeichnern im Head und Einheiten in Zeile mit Index 0
+    '''
     i = 0
     for value in list(data.head()):
         i += 1
@@ -60,23 +70,34 @@ def firstFormat(data):
         else:
             # @TODO: Frontend, verhindert leere Eingabe
             colNames_User.append(input('Wie möchten sie die Spalte ' + value + ' bennen? '))
-
     i = -1
-
     for value in list(data.head()):
         i += 1
         # @TODO: Frontend, Dropdown Liste
         colUnits_User.append(input('Bitte geben Sie die Einheit der Spalte ' + colNames_User[i] + ' ein: '))
 
     data.columns = colNames_User
-
     data.loc[-1] = colUnits_User
     data.index = data.index + 1
     data = data.sort_index()
     return data
 
 
+daten = firstFormat(masse)
 
-print(header_Format(daten2))
-print(daten.iloc[:,1])
+#lowpass filter
+b, a = sci.signal.butter(8, 0.225)
+#b, a = sci.signal.butter(2, 0.01,'low')
+#b, a = sci.signal.ellip(25,4, 1, 120, 5)  # Filter to be applied.
+filtered=sci.signal.filtfilt(a,b,daten.iloc[:,1][1:],padlen=150)
+
+plt.figure
+plt.plot(daten.iloc[:,0][1:], daten.iloc[:,1][1:], 'b', alpha=0.75)
+plt.plot(daten.iloc[:,0][1:], filtered, 'r')
+plt.legend(('noisy signal', 'filtfilt'), loc='best')
+plt.grid(True)
+plt.show()
+
+#Gauß Filter
+
 
