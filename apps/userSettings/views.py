8b11ea703a3de7e432ce47c1d66bb455050dd77b
maren.mehler@live.de <maren.mehler@live.de>
2017-12-06 16:35:36 +0100

    Versuch Einstellungen ändern, noch nicht getestet

--- apps/userSettings/views.py
+++ apps/userSettings/views.py
@@ -2,7 +2,17 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from apps.userSettings.forms import userSettingsForm
+
+
 def userSettings(request):
-   '''current_user = request.user
-    print(current_user)'''
+   #current_user = request.user
+   #print(current_user)
+   if request.method == 'POST':
+       form = userSettingsForm(data=request.POST, instance=request.user)
+       if form.is_valid():
+           user = form.save(commit=False)
+           user.save()
+           return render(request, 'userSettings/index.html')
    return render(request, 'userSettings/index.html')
+
