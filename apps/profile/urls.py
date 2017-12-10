93c573385a52e5d9e7dcffa4b3b33f4d4179195f
maren.mehler@live.de <maren.mehler@live.de>
2017-12-10 16:22:31 +0100

    apps/profile/urls.py geändert

--- apps/profile/urls.py
+++ apps/profile/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name='profile'
 urlpatterns = [
-    path('profile', views.show_me)
+    path('profile', views.show_me, name='profile')
     #hier Name der Funktion in views
     # für anderen User  name =<slug:token>
     ]
