960ce47a386d3914648f280a07904c6e38ac81e3
Aryan <aryan_o69@hotmail.de>
2017-12-10 17:48:50 +0100

    path settings

--- apps/userSettings/urls.py
+++ apps/userSettings/urls.py
@@ -1,14 +1,8 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.userSettings, name='index'),
-]
-'''from django.urls import path
+from django.urls import path
 
 from . import views
 
 app_name='userSettings'
 urlpatterns = [
-    path('userSettings', views.userSettings, name='userSettings')
-    ]'''
+    path('', views.userSettings, name='userSettings')
+    ]
