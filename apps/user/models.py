038394c14bd2f206206d6fbd3c15bba8be75c843
Huy-DC url=https://github.com/IT-PM-OpenAdaptronik/Webapp.git <khuynguyen96@gmail.com>
2017-12-11 12:45:27 +0100

    user profile visibility edited

--- apps/user/models.py
+++ apps/user/models.py
@@ -42,6 +42,10 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     company = models.CharField(max_length=255, null=True, blank=True)
     info = models.CharField(max_length=255, null=True, blank=True)
+    visibility_mail = models.BooleanField(default=False)
+    visibility_company = models.BooleanField(default=False)
+    visibility_info = models.BooleanField(default=False)
+
 
     USERNAME_FIELD = 'mail'
     EMAIL_FIELD = 'mail'
