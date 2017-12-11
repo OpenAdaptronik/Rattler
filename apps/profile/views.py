6a8195982de3467dff8066299accf0b7dc92f555
maren.mehler@live.de <maren.mehler@live.de>
2017-12-11 12:33:37 +0100

    profile/views geändert

--- apps/profile/views.py
+++ apps/profile/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def show_me(request):
-    respo = {'username': request.user.username, 'email': request.user.mail, 'company': request.user.company,
-             'infos': request.user.info}
+    #respo = {'username': request.user.username, 'email': request.user.mail, 'company': request.user.company,
+           #  'infos': request.user.info} wie in userSettings
 
     return render(request, 'profile/index.html', respo)
 
