1697368036514c5fba5d1bf488822f5a01c68b4b
maren.mehler@live.de <maren.mehler@live.de>
2017-12-11 11:42:19 +0100

    url zu path in userSettings

--- apps/userSettings/urls.py
+++ apps/userSettings/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     #url('changeEmail/', views.changeEmail, name='changeEmail'),
     #url(r'^changePassword/', views.ChangePassword, name='changePassword'),
     url(r'^$', views.userSettings, name='userSettings')
+    #path('userSettings', views.userSettings, name='userSettings')
     ]
