f7b2d347109800dee253fee3390618647246a9bf
Huy-DC url=https://github.com/IT-PM-OpenAdaptronik/Webapp.git <khuynguyen96@gmail.com>
2017-12-13 09:42:34 +0100

    fixed empty override problem

--- apps/userSettings/forms.py
+++ apps/userSettings/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from apps.user.models import User
 
+
 class UserSettingsForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['company', 'info']
+
+
+
+
--- apps/userSettings/views.py
+++ apps/userSettings/views.py
@@ -1,17 +1,28 @@
-from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 from apps.userSettings.forms import UserSettingsForm
 
 '''changes company and info'''
+
 def userSettings(request):
-   # respo = {'username': request.user.username, 'email': request.user.mail, 'company': request.user.company, 'infos': request.user.info}
+    # respo = {'username': request.user.username, 'email': request.user.mail,
+    # 'company': request.user.company, 'infos': request.user.info}
+    data1 = request.user.company
+    data2 = request.user.info
     if request.method == 'POST':
         form = UserSettingsForm(data=request.POST, instance=request.user)
-        user = form.save()
-        return render(request, 'userSettings/index.html')
+        if form.is_valid():
+            updated_data = request.POST.copy()
+            if form.cleaned_data['company'] is None:
+                updated_data['company'] = data1
+            if form.cleaned_data['info'] is None:
+                updated_data['info'] = data2
+            form = UserSettingsForm(data=updated_data, instance=request.user)
+            form.save()
     return render(request, 'userSettings/index.html')
+
+
 '''changes password'''
 def ChangePassword(request):
 
