d6ed0787491a4362de24e94379dbe3df5e4e3c91
Aryan <aryan_o69@hotmail.de>
2017-12-13 17:18:41 +0100

    email part1

--- apps/userSettings/forms.py
+++ apps/userSettings/forms.py
@@ -4,4 +4,4 @@ from apps.user.models import User
 class UserSettingsForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['visibility_mail', 'company', 'visibility_company','info','visibility_info']
+        fields = ['mail','visibility_mail', 'company', 'visibility_company','info','visibility_info']
--- apps/userSettings/views.py
+++ apps/userSettings/views.py
@@ -13,24 +13,33 @@ def userSettings(request):
         #prueft of Submit "Speichern" war
         if 'saveUser' in request.POST:
             #weil request.POST nicht ohne Fehlermeldung ueberschrieben oder kopiert werden kann
-            resp = {'visibility_mail': request.POST.get('visibility_mail'),
+            resp = {'visibility_mail': request.POST.get('visibilityEmail'),
                     'company': request.POST.get('company'),
-                    'visibility_company': request.POST.get('visibility_company'),
+                    'visibility_company': request.POST.get('visibilityCompany'),
                     'info' : request.POST.get('info'),
-                    'visibility_info':request.POST.get('visibility_info')
+                    'visibility_info':request.POST.get('visibilityInfo')
                     }
-            ''' checkboxen IM html dokument haben ihre Probleme
-            # Output Value '' --> 0 für Datenbank
+            #checkboxen IM html dokument haben ihre Probleme
+            # Output Value '' --> 0 fuer Datenbank
             if resp.get('visibility_mail') == '':
                 resp.update({'visibility_mail': '0'})
-            if resp.get('visibility_mail') == 'on':
-                resp.update({'visibility_company': '1'})'''
+
+            # Output Value '' --> 0 fuer Datenbank
+            if resp.get('visibility_company') == '':
+                resp.update({'visibility_company': '0'})
+
+            # Output Value '' --> 0 fuer Datenbank
+            if resp.get('visibility_info') == '':
+                resp.update({'visibility_info': '0'})
+
             #prueft auf leere feld "Firma"
             if resp.get('company') == '':
-                resp.update({'company' : request.user.company})
+                resp.update({'company': request.user.company})
+
             #prueft auf leere feld "info"
             if resp.get('info') == '':
                 resp.update({'info': request.user.info})
+
             form = UserSettingsForm(data=resp ,instance=request.user)
             if form.is_valid():
                 user = form.save()
@@ -62,5 +71,31 @@ def changePassword(request):
     })
 '''Email aendern'''
 def changeEmail(request):
- #noch in arbeit
-        return render(request, 'userSettings/changeEmail.html')
+    if request.method == 'POST':
+        mailer = {'mail': request.POST.get('newMail1'),
+                  'newMail2': request.POST.get('newMail2')
+                  }
+        #pueft auf verschiedene fehler
+        if not mailer.get('newMail2') == mailer.get('mail'):
+            messages.error(request, {'error': 'Email Adresse stimmt nicht überein!'})
+
+        else:
+
+
+            # sende mail, mit Token
+            return render(request,'Email wurde verschickt. Solange diese nicht bestätigt wurde beleibt die alte Email zum Login aktuell')
+
+
+
+    return render(request, 'userSettings/changeEmail.html')
+def changeEmailsucess (request,Token,mailer):
+
+    #check Token
+
+    # Falls token anufgerufen änder mail hiermit
+        form = UserSettingsForm(data=mailer, instance=request.user)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('/settings/')
+        return render(request, 'userSettings/index.html')
