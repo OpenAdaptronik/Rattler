12248a905d5c654552f6ad89ff52f0ef2c0e1857
maren.mehler@live.de <maren.mehler@live.de>
2017-12-21 14:21:01 +0100

    warten auf Link des Users auf Community und Filterfunktion

--- apps/profile/views.py
+++ apps/profile/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from apps.user.models import User
 
 # Create your views here.
 ''' shows user profile '''
 def show_me(request):
     #respo = {'username': request.user.username, 'email': request.user.mail, 'company': request.user.company,
            #  'infos': request.user.info} wie in userSettings
+    return render(request, 'profile/index.html')
 
-    return render(request, 'profile/index.html', respo)
 
-#def show_user(request, name):
-#    return
+
+def show_user(request, name):
+    requestUser = User.objects.get(username="")
+    return render(request, 'profile/index.html')
