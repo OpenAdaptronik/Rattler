171a6fea9f2e50c433c45fa792252be7b34dc7aa
Aryan <aryan_o69@hotmail.de>
2018-01-02 16:14:28 +0100

    OK

--- apps/profile/migrations/0001_initial.py
+++ apps/profile/migrations/0001_initial.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
+
     operations = [
         migrations.CreateModel(
             name='Profile',
