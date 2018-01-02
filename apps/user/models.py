0937d7902a5e3213b16b62d2741e1f6690d91d7e
alexander.kopp <kopp1990@googlemail.com>
2018-01-02 11:53:07 +0100

    super suer default to active

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (AbstractUser, UserManager as auth_UserManager)
 from django.utils.translation import gettext_lazy as _
 
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+
+class UserManager(auth_UserManager):
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        return super(UserManager, self).create_superuser(username, email, password, **extra_fields)
+
 
 class User(AbstractUser):
     email = models.EmailField(_('email address'))
     is_active = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
