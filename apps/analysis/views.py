25f69dcd27c68bdb57c36be346be025020d5836f
12jr <1203jr@gmx.de>
2018-01-04 14:24:52 +0100

    index.html aus process in analysis

--- apps/analysis/views.py
+++ apps/analysis/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
 from apps.calc.read_data import read
 from django.contrib.auth.decorators import login_required
-#import json
-#from .json import NumPyArangeEncoder
+import json
+from .json import NumPyArangeEncoder
 
 # Create your views here.
-
 @login_required
 def index(request):
     return render(request, "analysis/index.html")
