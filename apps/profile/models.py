5f5190db109fb3f9d699b25bdde5b1ce051aefc6
alexander.kopp <kopp1990@googlemail.com>
2018-01-04 11:55:26 +0100

    Translations

--- apps/profile/models.py
+++ apps/profile/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -24,6 +24,13 @@ class Profile(models.Model):
     max_datarows = models.IntegerField(default=1000)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
 
 def profile_image_path(instance, filename):
     return 'profile/%s%s' % (instance.profile.id, os.path.splitext(filename)[1])
