84be5e3a5c95ef90f9f35c7530a596e4c42e410c
maren.mehler@live.de <maren.mehler@live.de>
2018-01-04 13:39:42 +0100

    Seitenverlinkung Projekte nach neues Projekt und Details funktioniert

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'projects'
 urlpatterns = [
-    path('', views.save_project, name='index'),
+    path('', views.show_projects, name='showProjects'),
+   # path('', views.save_project, name='index'),
     path('new/', views.NewProject.as_view(), name='new'),
     path('detail/<slug:name>/<int:id>', views.detail, name='detail'),
 ]
