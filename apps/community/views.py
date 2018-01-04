f70ec36eb48ab53affde57943efee69f7d6f9405
maren.mehler@live.de <maren.mehler@live.de>
2018-01-04 16:51:08 +0100

    Filtern funktioniert, Aufgabe muss noch angepasst werden

--- apps/community/views.py
+++ apps/community/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from apps.user.models import User
 from apps.profile.models import Profile
+from apps.projects.models import Project
 
 
 '''Community'''
@@ -13,31 +14,46 @@ def user_filter(request):
         username = post_data['username']
         company = post_data['company']
         email = post_data['mail']
+        project_name = post_data['projectname']
+        category = post_data['category']
+        manufacturer = post_data['manufacturer']
         # empty search
-        if (username == '') and (email == '') and (company == ''):
-            return render(request, 'community/index.html', {'empty_search': 'Please enter something!'})
+        if (username == '') and (email == '') and (company == '') and (project_name == '') and (category == '') and (manufacturer == ''):
+            return render(request, 'community/index.html', {'empty_search': 'Bitte gib einen Suchbegriff ein!'})
         # legitimate search
-        filtered_ids = list(User.objects.values_list('id', flat=True))
+        filtered_ids = list(Project.objects.values_list('id', flat=True))
         if not(username == ''):
-            matching_ids = list(User.objects.filter(username__icontains=username).values_list('id', flat=True))
+            matching_ids = list(Project.objects.filter(user__username__icontains=username).values_list('id', flat=True))
             filtered_ids = list(set(matching_ids) & set(filtered_ids))
         if not(email == ''):
-            matching_ids = list(User.objects.filter(mail__icontains=email).values_list('id', flat=True))
+            matching_ids = list(Project.objects.filter(user__email__icontains=email).values_list('id', flat=True))
             filtered_ids = list(set(matching_ids) & set(filtered_ids))
         if not(company == ''):
-            matching_ids = list(Profile.objects.filter(company__icontains=company).values_list('userID_id', flat=True))
+            matching_ids = list(Project.objects.filter(user__profile__company__icontains=company).values_list('id', flat=True))
+            filtered_ids = list(set(matching_ids) & set(filtered_ids))
+        if not(project_name == ''):
+            matching_ids = list(Project.objects.filter(name__icontains=project_name).values_list('id', flat=True))
+            filtered_ids = list(set(matching_ids) & set(filtered_ids))
+        if not(category == ''):
+            matching_ids = list(Project.objects.filter(category__name__icontains=category).values_list('id', flat=True))
+            filtered_ids = list(set(matching_ids) & set(filtered_ids))
+        if not(manufacturer == ''):
+            matching_ids = list(Project.objects.filter(manufacturer__icontains=manufacturer).values_list('id', flat=True))
             filtered_ids = list(set(matching_ids) & set(filtered_ids))
         #return usernames in filtered_usernames
         i = 0
         filtered_usernames = list()
         while i < len(filtered_ids):
             currid = filtered_ids[i]
-            filtered_usernames.append(User.objects.get(id=currid).username)
+            filtered_usernames.append(Project.objects.get(id=currid).name)
             i += 1
+
+
+
         # no id matches
         if len(filtered_ids) == 0:
             return render(request, 'community/index.html', {'no_match': 'No user matches with your search, try again!'})
 
-        return render(request, 'community/index.html', {'filtered_usernames': filtered_usernames})
+        return render(request, 'community/index.html', {'filtered_ids': filtered_ids, 'filtered_usernames': filtered_usernames})
 
     return render(request, 'community/index.html')
