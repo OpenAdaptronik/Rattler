857505c7d257ee58205e2a77c2c03eca6c83b361
Aryan <aryan_o69@hotmail.de>
2018-01-18 16:16:07 +0100

    for Phil

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,18 +1,35 @@
 from django.contrib import admin
-from .models import Project, Category
+from .models import Project, Category,ProjectImage
+from django.utils import html
 from django.utils.translation import gettext_lazy as _
+from django.utils.encoding import force_text
+from django.urls import reverse
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'subcategory',  'manufacturer')
+    def get_project_image(instance):
+        if not instance.projectimage.path:
+            return
+        return html.format_html("""<img src="{src}" alt="{title}" style="max-width: 200px; max-height: 200px;" />""",
+                                src='/' + instance.projectimage.path.url,
+                                title=instance.name,
+                                )
+
+    list_display = ('name', 'category', 'subcategory', 'manufacturer', 'visibility')
     list_filter = ('category', 'subcategory')
-    readonly_fields=('created', 'updated')
+    readonly_fields = ('created', 'updated',
+                        get_project_image)
 
     fieldsets = (
         (None, {'fields': ('name', 'user', 'visibility')}),
-        (_('attributes'), {'fields': ('category', 'subcategory', 'manufacturer', 'typ',)}),
+        (_('attributes'), {'fields': ('category', 'subcategory', 'manufacturer', 'typ','description',)}),
         (_('Important dates'), {'fields': ('created', 'updated')}),
+        (_('Images'), {'fields': ('created', 'updated', get_project_image)}),
     )
+
+
+
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
