fd736526a9d623e032d0590d145beeeefb2e11c0
Heggi95 <joerg.hergert@gmail.com>
2018-01-18 17:27:41 +0100

    Trapeziodal Rule Integration works! need help ajax

--- apps/experiments/urls.py
+++ apps/experiments/urls.py
@@ -3,7 +3,8 @@ from apps.experiments import views
 
 app_name = 'experiments'
 urlpatterns = [
-    path('new/<int:id>', views.newE, name='new'),
     path('', views.index, name='index'),
-    #path('<int:id>', views.index, name='index'),
+    path('new/<int:id>', views.newE, name='new'),
+    path("newexperiments/intderivate" , views.intderivate ,name='intderivate'),
+    path('newexperiments/refresh', views.refreshData ,name='refresh'),
 ]
