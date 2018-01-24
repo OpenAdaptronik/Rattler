7fb4c9692661fbd7d2931a7945d8196292e050fc
alexander.kopp <kopp1990@googlemail.com>
2018-01-24 14:11:27 +0100

    #55 Datenauswahl punkte beschränken / infos einholen

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -7,4 +7,11 @@ from django.contrib.auth.decorators import login_required
 def show_projects(request):
     user_id = request.user.id
     my_projects = Project.objects.filter(user_id=user_id).values_list('name', flat=True)
-    return render(request, 'dashboard/index.html', {'my_projects': my_projects})
+    return render(
+        request,
+        'dashboard/index.html',
+        {
+            'my_projects': my_projects,
+            'max_datarows': request.user.profile.max_datarows,
+        }
+    )
